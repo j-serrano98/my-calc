802acaa9c5ac5a242df116b9c6bd5687fb1e34e8
Josue Serrano <jserrano.dev@hotmail.com>
2024-06-24 02:23:44 -0400

    Started Calculator GUI

--- scripts/my-calc.js
+++ scripts/my-calc.js
@@ -14,4 +14,23 @@ function divide(number1, number2) {
     return number1 / number2;
 }
 
+function operate(number1, number2, operator) {
+    switch (operator) {
+        case '+':
+            return add(number1, number2);
+        case '-':
+            return subtract(number1, number2);
+        case '*':
+            return multiply(number1, number2);
+        case '/':
+            return divide(number1, number2);
+        default:
+            return '0'
+    }
+}
+
+let number1 = undefined;
+let number2 = undefined;
+let operator = undefined;
+
 console.log(divide(75, 3))
